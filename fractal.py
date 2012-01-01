a9e04428a0ab07802174b23a1e296ea938a1bd08
Jonas Aaberg <cja@gmx.net>
2012-01-01 10:13:15 +0100

    Fractal generated landscape updated

--- fractal.py
+++ fractal.py
@@ -2,7 +2,12 @@
 import random
 
 class Fractal:
-    def __wrap(self, d):
+    def __dump(self):
+        if self.debug:
+            for x in range(0, self.size):
+                print self.terrain[x]
+
+    def __xwrap(self, d):
         if d >= self.size:
             return d - self.size + 1
         if d < 0:
@@ -15,12 +20,19 @@ class Fractal:
     def __get(self, x, y, s, p):
 
         if y < 0 or y >= self.size:
-            return s, p
+            return s, p + 1
 
-        if self.terrain[self.__wrap(x)][y] is not None:
-            return s + self.terrain[self.__wrap(x)][y], p + 1
+        if self.debug:
+            print x, "wrapped:", self.__xwrap(x), y, "is", self.terrain[self.__xwrap(x)][y]
+
+        if self.terrain[self.__xwrap(x)][y] is not None:
+            return s + self.terrain[self.__xwrap(x)][y], p + 1
         else:
             return s, p
+
+    def __set(self, x, y, val):
+        self.histogram[val] += 1
+        self.terrain[self.__xwrap(x)][y] = val
 
     def __square_avg(self, x, y, d, rand):
         s = 0
@@ -31,7 +43,10 @@ class Fractal:
         s, p = self.__get(x, y + d, s, p)
         s, p = self.__get(x, y - d, s, p)
 
-        return self.__add_rand(s / p, rand)
+        if x == 0 or x == self.size or y == 0 or y == self.size:
+            return 0
+        else:
+            return self.__add_rand(s / p, rand)
 
     def __generate(self, x, y, d, rand):
 
@@ -43,17 +58,21 @@ class Fractal:
         s, p = self.__get(x, y + d, s, p)
         s, p = self.__get(x + d, y + d, s, p)
 
-        self.terrain[x + d / 2][y + d / 2] = self.__add_rand(s / p, rand)
+        self.__set(x + d / 2, y + d / 2, self.__add_rand(s / p, rand))
 
         # square
-        self.terrain[x][y + d / 2] =  self.__square_avg(x, y + d/2, d / 2, rand)
-        self.terrain[x + d][y + d / 2] =  self.__square_avg(x + d, y + d/2, d / 2, rand)
-        self.terrain[x + d / 2][y] =  self.__square_avg(x + d / 2, y, d / 2, rand)
-        self.terrain[x + d / 2][y + d] =  self.__square_avg(x + d / 2, y + d, d /2, rand)
+        self.__set(self.__xwrap(x), y + d / 2, self.__square_avg(x, y + d/2, d / 2, rand))
+        self.__set(self.__xwrap(x + d), y + d / 2,  self.__square_avg(x + d, y + d/2, d / 2, rand))
+        self.__set(self.__xwrap(x + d / 2), y, self.__square_avg(x + d / 2, y, d / 2, rand))
+        self.__set(self.__xwrap(x + d / 2), y + d, self.__square_avg(x + d / 2, y + d, d /2, rand))
 
-    def __init__(self, size = 9):
+        self.__dump()
+
+    def __init__(self, size = 9, debug=False, update_callback=None):
         self.size = size
         self.terrain = []
+        self.histogram = 256 * [0]
+        self.debug = debug
 
         for y in range(0, size):
             l = []
@@ -61,27 +80,42 @@ class Fractal:
                 l.append(None)
             self.terrain.append(l)
 
-        self.terrain[0][0] = random.randint(0, 255)
-        self.terrain[0][size - 1] = random.randint(0, 255)
-        # Wrap east-west
-        self.terrain[size - 1][0] = self.terrain[0][0]
-        self.terrain[size - 1][size - 1] = self.terrain[0][size - 1]
+        self.__set(0, 0, 0)
+        self.__set(0, size - 1, 0)
+        self.__set(size - 1, 0, 0)
+        self.__set(size - 1, size - 1, 0)
+
+        #self.__set(0, 0, random.randint(0, 255))
+        #self.__set(0, size - 1, random.randint(0, 255))
+        # TODO: Wrap east-west. This is not good enough
+        #self.terrain[size - 1][0] = self.terrain[0][0]
+        #self.terrain[size - 1][size - 1] = self.terrain[0][size - 1]
 
         d = size - 1
         l = 1
         rand = 128
 
+        if update_callback:
+            update_callback(0, 5)
+
         while True:
             for m in range(0, l):
                 for n in range(0, l):
                     self.__generate(m*d, n*d, d, rand)
-            if d == 1:
+            if update_callback:
+                update_callback(d, 5)
+
+            if d == 2:
                 break
             l = l * 2
             d = d / 2
-            rand = rand / 2
-    def get_terrain(self):
+            # TODO: Parameter
+            rand = 65*rand/100
+    def getTerrain(self):
         return self.terrain
-    def get_size(self):
+    def getSize(self):
         return self.size
+    def getHistogram(self):
+        return self.histogram
+
 
--- test_fractal.py
+++ test_fractal.py
@@ -1,24 +1,73 @@
 #!/usr/bin/env python
 import pygame, time, sys 
+from pprint import pprint
 
 import fractal
 
-s = 257
+def update_callback(c, l):
+        sys.stdout.write("*")
+        sys.stdout.flush()
 
-d = fractal.Fractal(size=s)
-terrain = d.get_terrain();
+s = 256+1
+
+d = fractal.Fractal(size=s, debug=False, update_callback=update_callback)
+terrain = d.getTerrain()
+
+#pprint(terrain)
+
+#sys.exit(0)
+
+h = d.getHistogram()
+
+a = 0
+water_level = None
+tree_level = None
+hill_level = None
+mountain_level = None
+for i in range(0, 256):
+    a = a + h[i]
+    if a >= 80*sum(h)/100 and water_level is None:
+        water_level = i
+    if a >= 87*sum(h)/100 and tree_level is None:
+        tree_level = i
+    if a >= 96*sum(h)/100 and hill_level is None:
+        hill_level = i
+    if a >= 98*sum(h)/100 and mountain_level is None:
+        mountain_level = i
+print
+
+
 pygame.init()
 
-pygame.display.set_mode((s, s))
+pygame.display.set_mode((s*2, s*2))
 surface = pygame.display.get_surface()
+
+surface.fill((255,0,0), ((0, 0), (s*2,s*2)))
 
 
 for x in range(0, s):
     for y in range(0, s):
-        surface.fill((terrain[x][y],
-                      terrain[x][y],
-                      terrain[x][y]),
-                     ((x,y), (x,y)))
+        if 1:
+            if terrain[x][y] < water_level:
+                c = (0, 0, 255)
+            elif terrain[x][y] < tree_level:
+                c = (0, 255, 128)
+            elif terrain[x][y] < hill_level:
+                c = (0, 170, 0)
+            elif terrain[x][y] < mountain_level:
+                c = (170, 170, 170)
+            else:
+                c = (255, 255, 255)
+
+        else:
+            c = (terrain[x][y], terrain[x][y], terrain[x][y])
+
+        if x >= s/2:
+            mx = x - s/2
+        else:
+            mx = x + s/2
+
+        surface.set_at((mx,y), c)
 
 pygame.display.flip()
 
